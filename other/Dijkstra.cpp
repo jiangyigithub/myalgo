3f6dddb12b80276da983b2bedf1af515144c75f7
JIANG Yi (XC/PJ-ICV-CN) <jan5szh@SZH-C-005G6.szh.apac.bosch.com>
2023-07-04 16:39:09 +0800

    up

--- other/Dijkstra.cpp
+++ other/Dijkstra.cpp
@@ -3,7 +3,7 @@
 #include <queue>
 #include <iostream>
 
-void print(int s, int e, const std::vector<int> &parent)
+void print(int s, int e, const std::vector<int> &)
 {
     if (s == e)
         return;
@@ -103,17 +103,23 @@ void DijGraph::dijkstra(int s, int e)
 
 void testDijkstra()
 {
-    DijGraph g{6};
-    g.addEdge(0, 1, 1);
-    g.addEdge(0, 2, 12);
-    g.addEdge(1, 3, 3);
-    g.addEdge(1, 2, 9);
-    g.addEdge(3, 2, 4);
-    g.addEdge(3, 4, 13);
-    g.addEdge(2, 4, 5);
-    g.addEdge(4, 5, 4);
-    g.addEdge(3, 5, 15);
-    g.dijkstra(0, 5);
+    
+    DijGraph g{9}; // have direction
+    g.addEdge(0, 1, 4);
+    g.addEdge(0, 7, 8);
+    g.addEdge(1, 2, 8);
+    g.addEdge(1, 7, 11);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 8, 2);
+    g.addEdge(2, 5, 4);
+    g.addEdge(3, 4, 9);
+    g.addEdge(3, 5, 14);
+    g.addEdge(5, 4, 10);
+    g.addEdge(6, 5, 2);
+    g.addEdge(7, 6, 1);
+    g.addEdge(6, 8, 6);
+    g.addEdge(7, 8, 7);
+    g.dijkstra(0, 4);
 }
 
 int main()
