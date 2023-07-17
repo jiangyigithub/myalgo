ec0407cc17c5b6445e530e5ef61b89079dd759ee
yi jiang <yi.jiang3@bosch.com>
2023-07-17 10:40:19 +0800

    ++

--- cpp/11-18linked.cpp
+++ cpp/11-18linked.cpp
@@ -1,42 +1,51 @@
 #include "head.hpp"
 
-struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode(int x) : val(x), next(nullptr) {}
- };
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* deleteNode(ListNode* head, int val) {
+    ListNode *deleteNode(ListNode *head, int val)
+    {
         // is header
-        if(head->val == val) return head->next;
-        ListNode *pre = head,*cur = head->next;
-        while(cur->val!= val){
+        if (head->val == val)
+            return head->next;
+        ListNode *pre = head, *cur = head->next;
+        while (cur->val != val)
+        {
             pre = cur;
             cur = cur->next;
         }
         // is not tail
-        if(cur->next!=nullptr){
-            pre->next = cur->next;// 删除节点
+        if (cur->next != nullptr)
+        {
+            pre->next = cur->next; // 删除节点
         }
         return head;
     }
 };
 
-void trimLeftTrailingSpaces(string &input) {
-    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-        return !isspace(ch);
-    }));
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch)
+                                       { return !isspace(ch); }));
 }
 
-void trimRightTrailingSpaces(string &input) {
-    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-        return !isspace(ch);
-    }).base(), input.end());
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch)
+                        { return !isspace(ch); })
+                    .base(),
+                input.end());
 }
 
-vector<int> stringToIntegerVector(string input) {
+vector<int> stringToIntegerVector(string input)
+{
     vector<int> output;
     trimLeftTrailingSpaces(input);
     trimRightTrailingSpaces(input);
@@ -45,20 +54,23 @@ vector<int> stringToIntegerVector(string input) {
     ss.str(input);
     string item;
     char delim = ',';
-    while (getline(ss, item, delim)) {
+    while (getline(ss, item, delim))
+    {
         output.push_back(stoi(item));
     }
     return output;
 }
 
-ListNode* stringToListNode(string input) {
+ListNode *stringToListNode(string input)
+{
     // Generate list from the input
     vector<int> list = stringToIntegerVector(input);
 
     // Now convert that list into linked list
-    ListNode* dummyRoot = new ListNode(0);
-    ListNode* ptr = dummyRoot;
-    for(int item : list) {
+    ListNode *dummyRoot = new ListNode(0);
+    ListNode *ptr = dummyRoot;
+    for (int item : list)
+    {
         ptr->next = new ListNode(item);
         ptr = ptr->next;
     }
@@ -67,34 +79,37 @@ ListNode* stringToListNode(string input) {
     return ptr;
 }
 
-int stringToInteger(string input) {
+int stringToInteger(string input)
+{
     return stoi(input);
 }
 
-string listNodeToString(ListNode* node) {
-    if (node == nullptr) {
+string listNodeToString(ListNode *node)
+{
+    if (node == nullptr)
+    {
         return "[]";
     }
 
     string result;
-    while (node) {
+    while (node)
+    {
         result += to_string(node->val) + ", ";
         node = node->next;
     }
     return "[" + result.substr(0, result.length() - 2) + "]";
 }
 
-int main() {
+int main()
+{
     string line = "[4,5,1,9]";
-        ListNode* head = stringToListNode(line);
-        string lineval = "1";
-        int val = stringToInteger(lineval);
-        
-        ListNode* ret = Solution().deleteNode(head, val);
+    ListNode *head = stringToListNode(line);
+    string lineval = "1";
+    int val = stringToInteger(lineval);
 
-        string out = listNodeToString(ret);
-        cout << out << endl;
+    ListNode *ret = Solution().deleteNode(head, val);
+
+    string out = listNodeToString(ret);
+    cout << out << endl;
     return 0;
 }
-
-
