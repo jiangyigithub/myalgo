69478ccd189084e0ea3876dade3bfe2c9541e663
yi jiang <yi.jiang3@bosch.com>
2023-07-21 10:01:59 +0800

    ++

--- cpp/14-13search-dfs.cpp
+++ cpp/14-13search-dfs.cpp
@@ -1,30 +1,36 @@
 #include "head.hpp"
 
 // ===== Solution Code =====
-class Solution {
+class Solution
+{
 public:
-    int movingCount(int m, int n, int k) {
+    int movingCount(int m, int n, int k)
+    {
         vector<vector<bool>> visited(m, vector<bool>(n, 0));
         return dfs(0, 0, 0, 0, visited, m, n, k);
     }
+
 private:
-    int dfs(int i, int j, int si, int sj, vector<vector<bool>> &visited, int m, int n, int k) {
-        if(i >= m || j >= n || k < si + sj || visited[i][j]) return 0;
+    int dfs(int i, int j, int si, int sj, vector<vector<bool>> &visited, int m, int n, int k)
+    {
+        if (i >= m || j >= n || k < si + sj || visited[i][j])
+            return 0;
         visited[i][j] = true;
         return 1 + dfs(i + 1, j, (i + 1) % 10 != 0 ? si + 1 : si - 8, sj, visited, m, n, k) +
-                   dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8, visited, m, n, k);
+               dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8, visited, m, n, k);
     }
 };
 
-int main() {
+int main()
+{
     // ======= Test Case =======
     int m = 2;
     int n = 3;
     int k = 1;
     // ====== Driver Code ======
-    Solution* slt = new Solution();
+    Solution *slt = new Solution();
     int res = slt->movingCount(m, n, k);
     cout << res << endl;
-    
+
     return 0;
 }
