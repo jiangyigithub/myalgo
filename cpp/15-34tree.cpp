6fdf7d542642f7d9e4620f09539b55f7869f21cb
yi jiang <yi.jiang3@bosch.com>
2023-07-21 15:18:49 +0800

    ++

--- cpp/15-34tree.cpp
+++ cpp/15-34tree.cpp
@@ -1,57 +1,104 @@
+/*
+剑指 Offer 34. 二叉树中和为某一值的路径
+给你二叉树的根节点 root 和一个整数目标和 targetSum ，找出所有 从根节点到叶子节点 路径总和等于给定目标和的路径。
+
+叶子节点 是指没有子节点的节点。
+
+ 
+
+示例 1：
+
+
+
+输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+输出：[[5,4,11,2],[5,8,4,5]]
+示例 2：
+
+
+
+输入：root = [1,2,3], targetSum = 5
+输出：[]
+示例 3：
+
+输入：root = [1,2], targetSum = 0
+输出：[]
+ 
+
+提示：
+
+树中节点总数在范围 [0, 5000] 内
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+*/
+
+// 子集和问题：给定一个集合和一个目标和，找到集合中所有和为目标和的子集。
 #include "head.hpp"
- 
- struct TreeNode {
-      int val;
-      TreeNode *left;
-      TreeNode *right;
-      TreeNode(int node) : val(node), left(nullptr), right(nullptr) {}
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int node) : val(node), left(nullptr), right(nullptr) {}
 };
 
-
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+    vector<vector<int>> pathSum(TreeNode *root, int sum)
+    {
         recur(root, sum);
         return res;
     }
+
 private:
     vector<vector<int>> res;
     vector<int> path;
-    void recur(TreeNode* root, int tar) {
-        if(root == nullptr) return;
+
+private:
+    void recur(TreeNode *root, int tar)
+    {
+        if (root == nullptr)
+            return;
         path.push_back(root->val);
         tar -= root->val;
-        if(tar == 0 && root->left == nullptr && root->right == nullptr)
+        if (tar == 0 && root->left == nullptr && root->right == nullptr)
             res.push_back(path);
         recur(root->left, tar);
         recur(root->right, tar);
         path.pop_back();
     }
 };
-        //       5
-        //      / \
-        //     4   8
-        //    /   / \
-        //   11  13  4
-        //  /  \    / \
-        // 7    2  5   1
-void trimLeftTrailingSpaces(string &input) {
-    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
-        return !isspace(ch);
-    }));
+
+//       5
+//      / \
+//     4   8
+//    /   / \
+//   11  13  4
+//  /  \    / \
+// 7    2  5   1
+
+void trimLeftTrailingSpaces(string &input)
+{
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch)
+                                       { return !isspace(ch); }));
 }
 
-void trimRightTrailingSpaces(string &input) {
-    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
-        return !isspace(ch);
-    }).base(), input.end());
+void trimRightTrailingSpaces(string &input)
+{
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch)
+                        { return !isspace(ch); })
+                    .base(),
+                input.end());
 }
 
-TreeNode* stringToTreeNode(string input) {
+TreeNode *stringToTreeNode(string input)
+{
     trimLeftTrailingSpaces(input);
     trimRightTrailingSpaces(input);
     input = input.substr(1, input.length() - 2);
-    if (!input.size()) {
+    if (!input.size())
+    {
         return nullptr;
     }
 
@@ -60,31 +107,36 @@ TreeNode* stringToTreeNode(string input) {
     ss.str(input);
 
     getline(ss, item, ',');
-    TreeNode* root = new TreeNode(stoi(item));
-    queue<TreeNode*> nodeQueue;
+    TreeNode *root = new TreeNode(stoi(item));
+    queue<TreeNode *> nodeQueue;
     nodeQueue.push(root);
 
-    while (true) {
-        TreeNode* node = nodeQueue.front();
+    while (true)
+    {
+        TreeNode *node = nodeQueue.front();
         nodeQueue.pop();
 
-        if (!getline(ss, item, ',')) {
+        if (!getline(ss, item, ','))
+        {
             break;
         }
 
         trimLeftTrailingSpaces(item);
-        if (item != "null") {
+        if (item != "null")
+        {
             int leftNumber = stoi(item);
             node->left = new TreeNode(leftNumber);
             nodeQueue.push(node->left);
         }
 
-        if (!getline(ss, item, ',')) {
+        if (!getline(ss, item, ','))
+        {
             break;
         }
 
         trimLeftTrailingSpaces(item);
-        if (item != "null") {
+        if (item != "null")
+        {
             int rightNumber = stoi(item);
             node->right = new TreeNode(rightNumber);
             nodeQueue.push(node->right);
@@ -93,42 +145,56 @@ TreeNode* stringToTreeNode(string input) {
     return root;
 }
 
-int stringToInteger(string input) {
+int stringToInteger(string input)
+{
     return stoi(input);
 }
 
-string integerVectorToString(vector<int> list, int length = -1) {
-    if (length == -1) {
+string integerVectorToString(vector<int> list, int length = -1)
+{
+    if (length == -1)
+    {
         length = list.size();
     }
 
-    if (length == 0) {
+    if (length == 0)
+    {
         return "[]";
     }
 
     string result;
-    for(int index = 0; index < length; index++) {
+    for (int index = 0; index < length; index++)
+    {
         int number = list[index];
         result += to_string(number) + ", ";
     }
     return "[" + result.substr(0, result.length() - 2) + "]";
 }
 
-int main() {
+//       5
+//      / \
+//     4   8
+//    /   / \
+//   11  13  4
+//  /  \    / \
+// 7    2  5   1
+
+int main()
+{
     string line = "[5,4,8,11,null,13,4,7,2,null,null,5,1]";
-  
-    TreeNode* root = stringToTreeNode(line);
+
+    TreeNode *root = stringToTreeNode(line);
 
     line = "22";
 
     int target = stringToInteger(line);
-    
-    vector<vector<int>> rets = Solution().pathSum(root,target);
-    
-    for(auto ret:rets)
+
+    vector<vector<int>> rets = Solution().pathSum(root, target);
+
+    for (auto ret : rets)
     {
-    string out = integerVectorToString(ret);
-    cout << out << endl;
+        string out = integerVectorToString(ret);
+        cout << out << endl;
     }
     return 0;
 }
