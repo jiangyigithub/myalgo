264a28adb00a41e32f78684e228b8751f414ea40
yi jiang <yi.jiang3@bosch.com>
2023-08-09 13:55:36 +0800

    ++

--- cpp/17-40quicksort.cpp
+++ cpp/17-40quicksort.cpp
@@ -1,4 +1,5 @@
 #include "head.hpp"
+#include "PrintUtil.hpp"
 
 class Solution {
 public:
@@ -15,13 +16,29 @@ private:
         // 哨兵划分操作（以 arr[l] 作为基准数）
         int i = l, j = r;
         while (i < j) {
+            // Move j towards left until arr[j] < arr[l]
             while (i < j && arr[j] >= arr[l]) j--;
+             // Move i towards right until arr[i] > arr[l]
             while (i < j && arr[i] <= arr[l]) i++;
+            // Swap elements at indices i and j to maintain the partitioning
             swap(arr[i], arr[j]);
         }
+        // Place the pivot element in its correct sorted position
         swap(arr[i], arr[l]);
         // 递归左（右）子数组执行哨兵划分
         quickSort(arr, l, i - 1);
         quickSort(arr, i + 1, r);
     }
 };
+
+int main() {
+    // ======= Test Case =======
+    vector<int> arr = { 3, 2, 1 };
+    int k = 2;
+    // ====== Driver Code ======
+    Solution* slt = new Solution();
+    vector<int> res = slt->getLeastNumbers(arr, k);
+    PrintUtil::printVector(res);
+    
+    return 0;
+}
