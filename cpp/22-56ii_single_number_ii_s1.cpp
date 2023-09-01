13986f895cfa5355b37964e7afd2321778ac196e
yi jiang <yi.jiang3@bosch.com>
2023-09-01 10:37:36 +0800

    ++

--- cpp/22-56ii_single_number_ii_s1.cpp
+++ cpp/22-56ii_single_number_ii_s1.cpp
@@ -1,7 +1,18 @@
 /*
-* File: sfo_56ii_single_number_ii_s1.cpp
-* Created Time: 2021-12-09
-* Author: Krahets (krahets@163.com)
+剑指 Offer 56 - II. 数组中数字出现的次数 II
+在一个数组 nums 中除一个数字只出现一次之外，其他数字都出现了三次。请找出那个只出现一次的数字。
+
+ 
+
+示例 1：
+
+输入：nums = [3,4,3,3]
+输出：4
+示例 2：
+
+输入：nums = [9,1,7,9,7,9,7]
+输出：1
+
 */
 
 #include "include.hpp"
