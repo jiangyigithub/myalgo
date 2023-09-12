ecec4294679282874d160a1004cdf46b58539612
yi jiang <yi.jiang3@bosch.com>
2023-09-12 14:49:16 +0800

    ++

--- cpp/24-57ii_consecutive_numbers_with_sum_s_s2.cpp
+++ cpp/24-57ii_consecutive_numbers_with_sum_s_s2.cpp
@@ -1,7 +1,18 @@
 /*
-* File: sfo_57ii_consecutive_numbers_with_sum_s_s2.cpp
-* Created Time: 2021-12-09
-* Author: Krahets (krahets@163.com)
+剑指 Offer 57 - II. 和为 s 的连续正数序列
+输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+
+序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+
+示例 1：
+
+输入：target = 9
+输出：[[2,3,4],[4,5]]
+示例 2：
+
+输入：target = 15
+输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+
 */
 
 #include "include.hpp"
