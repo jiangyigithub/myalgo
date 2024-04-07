22ba8dbb8a73e3d12c204c18c2d2e0affcb47a83
yi jiang <yi.jiang3@bosch.com>
2024-04-07 16:53:31 +0800

    +

--- cpp/28-38tree copy 3.cpp
+++ cpp/28-38tree copy 3.cpp
@@ -13,19 +13,19 @@ public:
     {
         if (left == right)
         {
-            res.push_back(s);
+            res.push_back(s); // 添加排列方案
             return;
         }
         set<char> st;
         for (int i = left; i <= right; i++)
         {
-            if (st.find(s[i]) != st.end())
+            if (st.find(s[i]) != st.end()) // 重复，因此剪枝
                 continue;
             st.insert(s[i]);
 
-            std::swap(s[i], s[left]);
+            std::swap(s[i], s[left]); // 降低空间复杂度
             dfs(s, left + 1, right);
-            std::swap(s[i], s[left]);
+            std::swap(s[i], s[left]); // 回溯/撤销
         }
     }
 };
