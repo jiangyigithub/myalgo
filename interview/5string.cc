83ef54b3dec24f5600743f78c3bcf9af66d452b4
Jiang <yi.jiang3@cn.bosch.com>
2024-07-15 14:09:15 +0800

    +

--- interview/5string.cc
+++ interview/5string.cc
@@ -7,29 +7,44 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
+        // 如果输入字符串为空，直接返回空字符串
         if (s.empty()) return "";
+        
+        // 初始化起始位置和最大长度
         int start = 0, maxLen = 1;
 
+        // 遍历字符串中的每个字符
         for (int i = 0; i < s.size(); ++i) {
-            int len1 = expandAroundCenter(s, i, i);   // Odd length palindrome
-            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindrome
+            // 查找以单个字符为中心的奇数长度的回文
+            int len1 = expandAroundCenter(s, i, i);
+            
+            // 查找以两个字符为中心的偶数长度的回文
+            int len2 = expandAroundCenter(s, i, i + 1);
+            
+            // 选择较长的回文长度
             int len = max(len1, len2);
             
+            // 如果找到的回文长度大于当前最大长度
             if (len > maxLen) {
+                // 更新起始位置和最大长度
                 start = i - (len - 1) / 2;
                 maxLen = len;
             }
         }
 
+        // 返回最长回文子串
         return s.substr(start, maxLen);
     }
 
 private:
+    // 辅助函数：以中心为起点向外扩展寻找回文
     int expandAroundCenter(const string &s, int left, int right) {
+        // 当左右字符相等且在字符串范围内时，继续扩展
         while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            left--;
-            right++;
+            left--;  // 左边界左移
+            right++; // 右边界右移
         }
+        // 返回回文长度
         return right - left - 1;
     }
 };
