af244a681ba755f2f20c7c9963bf5c497ac5fd1f
Jiang <yi.jiang3@cn.bosch.com>
2024-07-18 17:22:46 +0800

    +

--- interview/438.cc
+++ interview/438.cc
@@ -17,8 +17,8 @@ struct TestCase {
 
 vector<int> findAnagrams(string s, string p) {
     vector<int> result;
-    int s_len = s.length();
-    int p_len = p.length();
+    int s_len = s.size();
+    int p_len = p.size();
     
     if (p_len > s_len) return result;
 
@@ -55,42 +55,8 @@ vector<int> findAnagrams(string s, string p) {
     return result;
 }
 
-vector<int> findAnagrams2(string s, string t) {
-    unordered_map<char, int> need, window;
-    for (char c : t) need[c]++;
-
-    int left = 0, right = 0;
-    int valid = 0;
-    vector<int> res; // 记录结果
-    while (right < s.size()) {
-        char c = s[right];
-        right++;
-        // 进行窗口内数据的一系列更新
-        if (need.count(c)) {
-            window[c]++;
-            if (window[c] == need[c]) 
-                valid++;
-        }
-        // 判断左侧窗口是否要收缩
-        while (right - left >= t.size()) {
-            // 当窗口符合条件时，把起始索引加入 res
-            if (valid == need.size())
-                res.push_back(left);
-            char d = s[left];
-            left++;
-            // 进行窗口内数据的一系列更新
-            if (need.count(d)) {
-                if (window[d] == need[d])
-                    valid--;
-                window[d]--;
-            }
-        }
-    }
-    return res;
-}
-
-vector<int> findAnagrams3(string s, string p) {
-        int n = s.length(), m = p.length();
+vector<int> findAnagrams2(string s, string p) {
+        int n = s.size(), m = p.size();
         vector<int> res;
         if (n < m) return res;
 
@@ -131,7 +97,7 @@ void runTestCases() {
     };
 
     for (auto& testCase : testCases) {
-        testCase.actual = findAnagrams3(testCase.s, testCase.p);
+        testCase.actual = findAnagrams2(testCase.s, testCase.p);
         testCase.passed = (testCase.actual == testCase.expected);
 
         std::cout << "Test case - s: \"" << testCase.s << "\", p: \"" << testCase.p << "\"\n";
