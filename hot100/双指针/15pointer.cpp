a5e34ab9c264a64d2d81ae948de864b407e12370
Jiang <yi.jiang3@cn.bosch.com>
2024-07-26 16:18:10 +0800

    +

--- hot100/双指针/15pointer.cpp
+++ hot100/双指针/15pointer.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
+using namespace std;
 /*
 该题特点: 无序 有重复 求所以解；应用的基础模版是双指针求两数之和
 基础模版：两数之和应用双指针的方法求和的前提得有序，因此，需先排序，再应用双指针
@@ -8,49 +10,60 @@
 这道题还要求要输出所有结果，所以求得结果后，还需要继续查找
 */
 // 函数计算和为 0 的三元组
-std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
-    std::vector<std::vector<int>> result;
+vector<std::vector<int>> threeSum(vector<int> &nums)
+{
+    vector<std::vector<int>> result;
     std::sort(nums.begin(), nums.end());
-    
-    for (int i = 0; i < nums.size(); ++i) {
-        if (i > 0 && nums[i] == nums[i-1]) {
+    int n = nums.size();
+    for (int i = 0; i < n - 2; ++i)
+    {
+        int x = nums[i];
+        if (i > 0 && nums[i] == nums[i - 1])
             continue;
-        }
+
         
         int left = i + 1;
-        int right = nums.size() - 1;
-        
-        while (left < right) {
+        int right = n - 1;
+
+        while (left < right)
+        {
             /// 先固定一个，再求两数之和(两数之和应用双指针的方法求和的前提得有序)
-            int sum = nums[i] + nums[left] + nums[right];
-            
-            if (sum == 0) {
+            int sum = x + nums[left] + nums[right];
+            if (sum > 0)
+            {
+                --right;
+            }
+            else if (sum < 0)
+            {
+                ++left;
+            }
+
+            else
+            {
                 /// 有多个解，怎么在找到一个解之后，继续求解
-                result.push_back({nums[i], nums[left], nums[right]});
-                
-                while (left < right && nums[left] == nums[left + 1]) {
+                result.push_back({x, nums[left], nums[right]});
+                ++left;
+                --right;
+
+                while (left < right && nums[left] == nums[left + 1])
+                {
                     ++left;
                 }
-                
-                while (left < right && nums[right] == nums[right - 1]) {
+
+                while (left < right && nums[right] == nums[right - 1])
+                {
                     --right;
                 }
-                
-                ++left;
-                --right;
-            } else if (sum < 0) {
-                ++left;
-            } else {
-                --right;
             }
         }
     }
-    
+
     return result;
 }
 
 // 结构体存储测试用例
-struct TestCase {
+struct TestCase
+{
     std::vector<int> nums;
     std::vector<std::vector<int>> expected;
     std::vector<std::vector<int>> actual;
@@ -58,31 +71,41 @@ struct TestCase {
 };
 
 // 检查两个二维向量是否相同
-bool compareVectors(const std::vector<std::vector<int>>& a, const std::vector<std::vector<int>>& b) {
-    if (a.size() != b.size()) return false;
-    for (int i = 0; i < a.size(); ++i) {
-        if (a[i] != b[i]) return false;
+bool compareVectors(const std::vector<std::vector<int>> &a, const std::vector<std::vector<int>> &b)
+{
+    if (a.size() != b.size())
+        return false;
+    for (int i = 0; i < a.size(); ++i)
+    {
+        if (a[i] != b[i])
+            return false;
     }
     return true;
 }
 
 // 打印二维向量
-void printVector(const std::vector<std::vector<int>>& vec) {
+void printVector(const std::vector<std::vector<int>> &vec)
+{
     std::cout << "[";
-    for (int i = 0; i < vec.size(); ++i) {
+    for (int i = 0; i < vec.size(); ++i)
+    {
         std::cout << "[";
-        for (int j = 0; j < vec[i].size(); ++j) {
+        for (int j = 0; j < vec[i].size(); ++j)
+        {
             std::cout << vec[i][j];
-            if (j < vec[i].size() - 1) std::cout << ", ";
+            if (j < vec[i].size() - 1)
+                std::cout << ", ";
         }
         std::cout << "]";
-        if (i < vec.size() - 1) std::cout << ", ";
+        if (i < vec.size() - 1)
+            std::cout << ", ";
     }
     std::cout << "]";
 }
 
 // 运行测试用例并输出结果
-void runTestCases() {
+void runTestCases()
+{
     std::vector<TestCase> testCases = {
         {{-1, 0, 1, 2, -1, -4}, {{-1, -1, 2}, {-1, 0, 1}}, {}, false},
         {{0, 1, 1}, {}, {}, false},
@@ -90,13 +113,15 @@ void runTestCases() {
         {{-2, 0, 1, 1, 2}, {{-2, 0, 2}, {-2, 1, 1}}, {}, false},
         // 添加更多测试用例
     };
-    
-    for (auto& test : testCases) {
+
+    for (auto &test : testCases)
+    {
         test.actual = threeSum(test.nums);
         test.passed = compareVectors(test.actual, test.expected);
-        
+
         std::cout << "Test Case - nums: ";
-        for (int num : test.nums) {
+        for (int num : test.nums)
+        {
             std::cout << num << " ";
         }
         std::cout << "\nExpected: ";
@@ -107,7 +132,8 @@ void runTestCases() {
     }
 }
 
-int main() {
+int main()
+{
     runTestCases();
     return 0;
 }
