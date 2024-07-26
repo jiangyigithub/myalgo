52b92bfce44ba5ac9e7798fec62313823e8c2c9b
Jiang <yi.jiang3@cn.bosch.com>
2024-07-26 22:47:09 +0800

    +

--- hot100/链表/146.py
+++ hot100/链表/146.py
@@ -9,15 +9,15 @@ class LRUCache(collections.OrderedDict):
     def get(self, key: int) -> int:
         if key not in self:
             return -1
-        self.move_to_end(key)
+        self.move_to_end(key) # 将访问的键移动到末尾，标记为最近使用
         return self[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self:
-            self.move_to_end(key)
+            self.move_to_end(key)  # 如果键已存在，移动到末尾
         self[key] = value
         if len(self) > self.capacity:
-            self.popitem(last=False)
+            self.popitem(last=False)  # 移除最前面的键值对，即最久未使用的
 
 def test_lru_cache():
     # 创建容量为2的LRU缓存
@@ -50,4 +50,30 @@ def test_lru_cache():
     print("所有测试用例通过！")
 
 # 运行测试用例
+# 创建一个有序字典
+od = collections.OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+print(od)  # OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+
+# 将键 'b' 移动到末尾
+od.move_to_end('b')
+print(od)  # OrderedDict([('a', 1), ('c', 3), ('b', 2)])
+
+# 将键 'a' 移动到开头
+od.move_to_end('a', last=False)
+print(od)  # OrderedDict([('a', 1), ('c', 3), ('b', 2)])
+
+# 创建一个有序字典
+od2 = collections.OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+print(od2)  # OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+
+# 删除并返回最后一个键值对
+item = od2.popitem()
+print(item)  # ('c', 3)
+print(od2)  # OrderedDict([('a', 1), ('b', 2)])
+
+# 删除并返回第一个键值对
+item = od2.popitem(last=False)
+print(item)  # ('a', 1)
+print(od2)  # OrderedDict([('b', 2)])
+
 test_lru_cache()
