94e39a61d71f66128e9ec5c6c9fb6b9799a415f0
Jiang <yi.jiang3@cn.bosch.com>
2024-07-28 14:46:43 +0800

    +

--- hot100/链表/146.3 iterator.cc
+++ hot100/链表/146.3 iterator.cc
@@ -6,38 +6,123 @@
 #include <utility> // for std::pair
 using namespace std;
 // LRUCache class definition
-class LRUCache {
+
+class Node
+{
 public:
-    LRUCache(int capacity) : cap(capacity) {
-    }
+    int key, value;
+    Node *prev, *next;
 
-    int get(int key) {
-        if (map.find(key) == map.end()) return -1;
-        auto key_value = *map[key];
-        cache.erase(map[key]);
-        cache.push_front(key_value);
-        map[key] = cache.begin();
-        return key_value.second;
-    }
-
-    void put(int key, int value) {
-        if (map.find(key) == map.end()) {
-            if (cache.size() == cap) {
-                map.erase(cache.back().first);
-                cache.pop_back();
-            }
-        }
-        else {
-            cache.erase(map[key]);
-        }
-        cache.push_front({key, value});
-        map[key] = cache.begin();
-    }
-private:
-    int cap;
-    list<pair<int, int>> cache;
-    unordered_map<int, list<pair<int, int>>::iterator> map;
+    Node(int k = 0, int v = 0) : key(k), value(v) {}
 };
+
+class LRUCache
+{
+private:
+    int capacity;
+    Node *dummy; // 哨兵节点
+    unordered_map<int, Node *> hashMap;
+
+    // 删除一个节点（抽出一本书）
+    void remove(Node *x)
+    {
+        x->prev->next = x->next;
+        x->next->prev = x->prev;
+    }
+
+    // 在链表头添加一个节点（把一本书放在最上面）
+    void push_front(Node *x)
+    {
+        x->prev = dummy;
+        x->next = dummy->next;
+        x->prev->next = x;
+        x->next->prev = x;
+    }
+
+    /// @brief get_node 操作不会影响hashMap，通过key怎么找到节点
+    /// @param key 
+    /// @return 
+    Node *get_node(int key)
+    {
+        if (hashMap.find(key) != hashMap.end())
+        {
+            auto node = hashMap[key]; // 有这本书
+            // 更新双端队列
+            remove(node);             // 把这本书抽出来
+            push_front(node);         // 放在最上面
+            return node;
+        }
+        return nullptr;
+    }
+
+public:
+    LRUCache(int capacity) : capacity(capacity), dummy(new Node())
+    {
+        dummy->prev = dummy;
+        dummy->next = dummy;
+    }
+
+    int get(int key)
+    {
+        auto node = get_node(key);
+        return node ? node->value : -1;
+    }
+
+    void put(int key, int value)
+    {
+        auto node = get_node(key);
+        // 1. 有这本书更新值
+        if (node)
+        {                        // 有这本书
+            node->value = value; // 更新 value
+            return;
+        }
+        // 2. 没有这本书新建节点
+        hashMap[key] = node = new Node(key, value); // 新书
+        push_front(node); // 放在最上面
+        // 3. 书太多了，删除老节点                         
+        if (hashMap.size() > capacity)
+        { 
+            auto back_node = dummy->prev;
+            hashMap.erase(back_node->key);
+            remove(back_node); // 去掉最后一本书
+            delete back_node;  // 释放内存
+        }
+    }
+};
+
+// class LRUCache {
+// public:
+//     LRUCache(int capacity) : cap(capacity) {
+//     }
+
+//     int get(int key) {
+//         if (map.find(key) == map.end()) return -1;
+//         auto key_value = *map[key];
+//         cache.erase(map[key]);
+//         cache.push_front(key_value);
+//         map[key] = cache.begin();
+//         return key_value.second;
+//     }
+
+//     void put(int key, int value) {
+//         if (map.find(key) == map.end()) {
+//             if (cache.size() == cap) {
+//                 map.erase(cache.back().first);
+//                 cache.pop_back();
+//             }
+//         }
+//         else {
+//             cache.erase(map[key]);
+//         }
+//         cache.push_front({key, value});
+//         map[key] = cache.begin();
+//     }
+// private:
+//     int cap;
+//     list<pair<int, int>> cache;
+//     unordered_map<int, list<pair<int, int>>::iterator> map;
+// };
 
 
 // TestCase structure definition
