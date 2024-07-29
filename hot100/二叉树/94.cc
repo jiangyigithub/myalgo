6b5b3f76032c59958b5e13190c039c34779655e1
Jiang <yi.jiang3@cn.bosch.com>
2024-07-29 15:16:29 +0800

    +

--- hot100/二叉树/94.cc
+++ hot100/二叉树/94.cc
@@ -1,3 +1,8 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <queue>
+using namespace std;
 
 struct TreeNode
 {
@@ -14,46 +19,56 @@ class Solution
 public:
     vector<int> inorderTraversal(TreeNode *root)
     {
-        if(root==nullptr) return {};
-        vector<int> ans = inorderTraversal(root->left);
-        ans.push_back(root->val)
-        auto temp = inorderTraversal(root->right);
-        ans.insert(ans.end(),temp.begin(),temp.end());
+
+        vector<int> ans;
+        dfs(root, ans);
         return ans;
+    }
+
+    void dfs(TreeNode *root, vector<int>& ans)// 传入引用
+    {
+        if (root == nullptr)
+            return;
+        dfs(root->left, ans);
+        ans.push_back(root->val);
+        dfs(root->right, ans);
     }
 };
 
 // Helper function to create a binary tree node
-TreeNode* createNode(int val) {
+TreeNode *createNode(int val)
+{
     return new TreeNode(val);
 }
 
-int main() {
+int main()
+{
     // Creating a binary tree for testing:
     //      1
     //       \
     //        2
     //       /
     //      3
-    TreeNode* root = createNode(1);
+    TreeNode *root = createNode(1);
     root->right = createNode(2);
     root->right->left = createNode(3);
-    
+
     Solution solution;
     vector<int> result = solution.inorderTraversal(root);
     cout << "Recursive In-order Traversal: ";
-    for (int val : result) {
+    for (int val : result)
+    {
         cout << val << " ";
     }
     cout << endl;
-    
-    SolutionIterative solutionIterative;
-    result = solutionIterative.inorderTraversal(root);
+
+
     cout << "Iterative In-order Traversal: ";
-    for (int val : result) {
+    for (int val : result)
+    {
         cout << val << " ";
     }
     cout << endl;
-    
+
     return 0;
 }
