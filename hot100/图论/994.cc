648386660f527c8e82685d036b38c3b877387424
Jiang <yi.jiang3@cn.bosch.com>
2024-07-31 22:27:14 +0800

    +

--- hot100/图论/994.cc
+++ hot100/图论/994.cc
@@ -11,32 +11,32 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         int fresh = 0;
-        vector<pair<int, int>> q;
+        vector<pair<int, int>> cur;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
                     fresh++; // 统计新鲜橘子个数
                 } else if (grid[i][j] == 2) {
-                    q.emplace_back(i, j); // 一开始就腐烂的橘子
+                    cur.push_back(i, j); // 一开始就腐烂的橘子
                 }
             }
         }
 
         int ans = -1;
-        while (!q.empty()) {
+        while (!cur.empty()) {
             ans++; // 经过一分钟
             vector<pair<int, int>> nxt;
-            for (auto& [x, y] : q) { // 已经腐烂的橘子
+            for (auto& [x, y] : cur) { // 已经腐烂的橘子
                 for (auto d : DIRECTIONS) { // 四方向
                     int i = x + d[0], j = y + d[1];
                     if (0 <= i && i < m && 0 <= j && j < n && grid[i][j] == 1) { // 新鲜橘子
                         fresh--;
                         grid[i][j] = 2; // 变成腐烂橘子
-                        nxt.emplace_back(i, j);
+                        nxt.push_back(i, j);
                     }
                 }
             }
-            q = move(nxt);
+            cur = nxt;
         }
 
         return fresh ? -1 : max(ans, 0);// 最后如果发现 fresh>0，就意味着有橘子永远不会腐烂，返回 −1
@@ -56,10 +56,10 @@ void runTests() {
     // Create test cases
     vector<TestCase> testCases;
 
-    testCases.emplace_back(vector<vector<int>>{{2,1,1},{1,1,0},{0,1,1}}, 4);
-    testCases.emplace_back(vector<vector<int>>{{2,1,1},{0,1,1},{1,0,1}}, -1);
-    testCases.emplace_back(vector<vector<int>>{{0,2}}, 0);
-    testCases.emplace_back(vector<vector<int>>{{2,2,2,1,1},{1,1,1,1,2},{2,2,1,2,1}}, 2);
+    testCases.push_back(vector<vector<int>>{{2,1,1},{1,1,0},{0,1,1}}, 4);
+    testCases.push_back(vector<vector<int>>{{2,1,1},{0,1,1},{1,0,1}}, -1);
+    testCases.push_back(vector<vector<int>>{{0,2}}, 0);
+    testCases.push_back(vector<vector<int>>{{2,2,2,1,1},{1,1,1,1,2},{2,2,1,2,1}}, 2);
 
     Solution sol;
     for (size_t i = 0; i < testCases.size(); ++i) {
