2b70b9680b49ce286aee13b15dab316e34c79380
Jiang <yi.jiang3@cn.bosch.com>
2024-08-17 17:01:29 +0800

    +

--- hot100/栈/739.cc
+++ hot100/栈/739.cc
@@ -39,6 +39,39 @@ public:
 
         return ans;
     }
+
+    vector<int> dailyTemperaturesWithRight(vector<int> &temperatures) {
+
+        int n = temperatures.size();
+
+        vector<int> ans(n);
+
+        stack<int> st;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int t = temperatures[i];
+
+            while (!st.empty() && t >= temperatures[st.top()]) {
+
+                st.pop();
+
+            }
+
+            if (!st.empty()) {
+
+                ans[i] = st.top() - i;
+
+            }
+
+            st.push(i);
+
+        }
+
+        return ans;
+
+    }
+
 };
 
 // 定义结构体来存储测试用例
