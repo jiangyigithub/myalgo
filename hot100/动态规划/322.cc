601edfdefcb9e5d598051cdc571744504ce015b4
Jiang <yi.jiang3@cn.bosch.com>
2024-09-02 15:27:47 +0800

    +

--- hot100/动态规划/322.cc
+++ hot100/动态规划/322.cc
@@ -10,76 +10,98 @@ using namespace std;
 // {{2}, 1, -1},           // Test case 5: 1 cannot be made with only coin 2
 // {{1, 2, 5, 10}, 27, 4}  // Test case 6: 27 can be made with 10 + 10 + 5 + 2
 
-class Solution {
+class Solution
+{
 
 public:
-
-    int coinChange(vector<int>& coins, int amount) {
-
+    int coinChange(vector<int> &coins, int amount)
+    {
         int n = coins.size();
-
         vector<vector<int>> memo(n, vector<int>(amount + 1, -1)); // -1 表示没有访问过
-
-        auto dfs = [&](auto&& dfs, int i, int c) -> int {
-
-            if (i < 0) {
-
+        auto dfs = [&](auto &&dfs, int i, int c) -> int
+        {
+            if (i < 0)
+            {
                 return c == 0 ? 0 : INT_MAX / 2; // 除 2 防止下面 + 1 溢出
-
             }
-
             int &res = memo[i][c]; // 注意这里是引用
-
-            if (res != -1) { // 之前计算过
-
+            if (res != -1)
+            { // 之前计算过
                 return res;
-
             }
-
-            if (c < coins[i]) {
-
+            if (c < coins[i])
+            {
                 return res = dfs(dfs, i - 1, c);
-
             }
-
             return res = min(dfs(dfs, i - 1, c), dfs(dfs, i, c - coins[i]) + 1);
-
         };
-
         int ans = dfs(dfs, n - 1, amount);
-
         return ans < INT_MAX / 2 ? ans : -1;
-
     }
 
+    /* 零钱兑换：动态规划 */
+    int coinChangeDP(vector<int> &coins, int amount)
+    {
+        int n = coins.size();
+        int MAX = amount + 1;
+        // 初始化 dp 表
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+        // 状态转移：首行首列
+        for (int a = 1; a <= amount; a++)
+        {
+            dp[0][a] = MAX;
+        }
+        // 状态转移：其余行和列
+        for (int i = 1; i <= n; i++)
+        {
+            for (int a = 1; a <= amount; a++)
+            {
+                if (coins[i - 1] > a)
+                {
+                    // 若超过目标金额，则不选硬币 i
+                    dp[i][a] = dp[i - 1][a];
+                }
+                else
+                {
+                    // 不选和选硬币 i 这两种方案的较小值
+                    dp[i][a] = min(dp[i - 1][a], dp[i][a - coins[i - 1]] + 1);
+                }
+            }
+        }
+        return dp[n][amount] != MAX ? dp[n][amount] : -1;
+    }
 };
 
-
-struct TestCase {
+struct TestCase
+{
     vector<int> coins;
     int amount;
     int expected;
 };
 
-int main() {
+int main()
+{
     TestCase testCases[] = {
         {{1, 2, 5}, 11, 3},     // Test case 1: 11 can be made with 5 + 5 + 1
         {{2}, 3, -1},           // Test case 2: 3 cannot be made with only coin 2
         {{1, 2, 5}, 0, 0},      // Test case 3: 0 can be made with no coins
         {{1}, 2, 2},            // Test case 4: 2 can be made with two 1 coins
         {{2}, 1, -1},           // Test case 5: 1 cannot be made with only coin 2
-        {{1, 2, 5, 10}, 27, 4},  // Test case 6: 27 can be made with 10 + 10 + 5 + 2
-        {{5,4,3},20,4}
-    };
+        {{1, 2, 5, 10}, 27, 4}, // Test case 6: 27 can be made with 10 + 10 + 5 + 2
+        {{5, 4, 3}, 20, 4}};
 
-    for (size_t i = 0; i < sizeof(testCases) / sizeof(TestCase); ++i) {
-        const TestCase& testCase = testCases[i];
+    for (size_t i = 0; i < sizeof(testCases) / sizeof(TestCase); ++i)
+    {
+        const TestCase &testCase = testCases[i];
         vector<int> temp = testCase.coins;
         int result = Solution().coinChange(temp, testCase.amount);
         cout << "Test case " << i + 1 << ": expected = " << testCase.expected << ", got = " << result;
-        if (result == testCase.expected) {
+        if (result == testCase.expected)
+        {
             cout << " [PASSED]" << endl;
-        } else {
+        }
+        else
+        {
             cout << " [FAILED]" << endl;
         }
     }
