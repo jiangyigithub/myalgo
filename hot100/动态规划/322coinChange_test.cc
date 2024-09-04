454249dacd8f2b7711e0458abdb9de70d9eedfaf
Jiang <yi.jiang3@cn.bosch.com>
2024-09-04 09:38:59 +0800

    +

--- hot100/动态规划/322coinChange_test.cc
+++ hot100/动态规划/322coinChange_test.cc
@@ -29,9 +29,11 @@ int coinChange(const vector<int>& coins, int amount)
 {
     vector<int> dp(amount+1,amount+1);
     dp[0]=0;
-    for(int i=1;i<=amount;++i){
-        for(int coin:coins){
-            if(i-coin>=0){
+    for(int coin:coins)
+    {
+        for(int i=coin;i<=amount;++i)
+        {
+            {
                 dp[i]=min(dp[i],dp[i-coin]+1);
             }
         }
