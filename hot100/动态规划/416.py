49ac174126d2ebbea615c2d1e18911e11bf9d2f1
Jiang <yi.jiang3@cn.bosch.com>
2024-09-05 13:30:36 +0800

    +

--- hot100/动态规划/416.py
+++ hot100/动态规划/416.py
@@ -22,6 +22,5 @@ class Solution:
             if i < 0:
                 return j == 0
             return j >= nums[i] and dfs(i - 1, j - nums[i]) or dfs(i - 1, j)
-
         s = sum(nums)
         return s % 2 == 0 and dfs(len(nums) - 1, s // 2)
