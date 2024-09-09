fdd98c1d988a24b6ee6eaa18449910bc87a03f7a
Jiang <yi.jiang3@cn.bosch.com>
2024-09-09 12:47:46 +0800

    +

--- hot100/回溯/51.py
+++ hot100/回溯/51.py
@@ -23,6 +23,27 @@ class Solution:
                     dfs(r+1,s-{c})
         dfs(0,set(range(n)))
         return ans
+    
+    def solveNQueens2(self, n: int) -> List[List[str]]:
+
+        m = n * 2 - 1
+        ans = []
+        col = [0] * n
+        on_path, diag1, diag2 = [False] * n, [False] * m, [False] * m
+        
+        def dfs(r: int) -> None:
+            if r == n:
+                ans.append(['.' * c + 'Q' + '.' * (n - 1 - c) for c in col])
+                return
+            for c, on in enumerate(on_path):
+                if not on and not diag1[r + c] and not diag2[r - c]:
+                    col[r] = c
+                    on_path[c] = diag1[r + c] = diag2[r - c] = True
+                    dfs(r + 1)
+                    on_path[c] = diag1[r + c] = diag2[r - c] = False  # 恢复现场
+        dfs(0)
+        return ans
+
 
 # 定义测试用例结构体
 class TestCase:
