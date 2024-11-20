7eb1201e622408d6834858d8664875339a64ed27
Jiang <yi.jiang3@cn.bosch.com>
2024-11-20 11:23:50 +0800

    +

--- lin/987.py
+++ lin/987.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from typing import List, Optional
+from math import inf
 
 # 定义树节点
 class TreeNode:
@@ -10,6 +11,27 @@ class TreeNode:
 
 # 解决方案类
 class Solution:
+    def verticalTraversal2(self, root: Optional[TreeNode]) -> List[List[int]]:
+        data = []
+        def dfs(node: Optional[TreeNode], row: int, col: int):
+            if node is None:
+                return
+            data.append((col, row, node.val))
+            dfs(node.left, row + 1, col - 1)
+            dfs(node.right, row + 1, col + 1)
+        dfs(root, 0, 0)
+
+        ans = []
+        last_col = inf
+        data.sort()
+        for col, _, val in data:
+            if col != last_col:
+                last_col = col
+                ans.append([])
+            ans[-1].append(val)
+        return ans
+
+
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
         groups = defaultdict(list)  # 哈希表存储 (col -> [(row, val)])
         
